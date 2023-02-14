8c2e4af526000d4afae7a084b3d2bc2d6b740e43
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-14 17:31:03 -0500

    u

--- test_accel.py
+++ test_accel.py
@@ -7,9 +7,9 @@ def main():
     accel.calibrate()
     accel.getSamplingFreq()
     while True:
-        accel.printAccel()
+        #accel.printAccel()
         #print(accel.getSpeed())
-        #thx,thy =accel.getAngles()
+        thx,thy =accel.getAngles()
         #print(thx, thy)
 
 main()
