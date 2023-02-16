daa9ff29ec83d56f16236de1e6a7d82e4b85db92
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-16 17:46:21 -0500

    u

--- accelerometer.py
+++ accelerometer.py
@@ -28,9 +28,9 @@ class Accelerometer:
         self.vy = 0
         self.vz = 0
 
-        self.axOff = 0.30476047799999745
-        self.ayOff = 0.30476047799999745
-        self.azOff = 0.12532269
+        self.axOff = 0.19832264209559977
+        self.ayOff = 0.19832264209559977
+        self.azOff = 0.27480340153799954
 
         self.freq= self.getSamplingFreq()
         self.scale = 0
@@ -101,7 +101,7 @@ class Accelerometer:
             # Scaling values
             self.axOff += rawAx*self.scale
             self.ayOff += rawAy*self.scale
-            self.azOff += rawAz*self.scale + g
+            self.azOff += rawAz*self.scale  + g
             
             cntOff += 1
         # Getting offsets
