5e83ba17afdbb44f73c3e5c6c0806b27a7e6907e
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 16:01:01 -0500

    u

--- start.py
+++ start.py
@@ -1,7 +1,14 @@
 import can
 
-can_interface = 'can0'  # nombre de la interfaz CAN
-bus = can.interface.Bus(can_interface, bustype='socketcan', bitrate=125000)  # inicialización del bus CAN
+# Configura el bus CAN
+can_interface = 'can0'
+bus = can.interface.Bus(can_interface, bustype='socketcan')
 
-message = can.Message(arbitration_id=0xA, data=[0x01, 0x1F, 0x80, 0x08, 0x01, 0x00, 0x00, 0x00], is_extended_id=False)  # creación del mensaje para ejecutar Autostart a 1
-bus.send(message)  # envío del mensaje al bus CAN
+# Define el mensaje CAN
+msg = can.Message(arbitration_id=0xA, data=[0x1F, 0x80, 0x1F, 0x08, 0x01, 0x00, 0x00, 0x00], extended_id=False)
+
+# Envía el mensaje CAN
+bus.send(msg)
+
+# Cierra el bus CAN
+bus.shutdown()
