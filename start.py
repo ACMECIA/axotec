989c8e115fdca4f5b3a2dab46a4d4413112064dd
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 16:08:59 -0500

    u

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ import canopen
 network = canopen.Network()
 network.connect(bustype='socketcan', channel='can0', bitrate=125000)
 node_id = 10
-node = network.add_node(node_id, 'EDS_FILE.eds')
+node = network.add_node(node_id, 'JN2100_V2.5.4.eds')
 
 # Escribir el valor 1 en el sub-índice 0x08 del índice 0x1F80
 node.sdo[0x1F80][0x08].raw = 1
