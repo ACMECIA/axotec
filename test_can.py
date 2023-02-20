6394b5df1cb8c2709cabd497af736fbb35380499
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 14:28:18 -0500

    u

---
+++ test_can.py
@@ -0,0 +1,25 @@
+
+from can import CAN
+import os, time
+
+
+# CAN configuration
+
+can_frame_fmt = "=IB3x8s"
+
+can_port = "can0"
+
+can = CAN(can_frame_fmt, can_port)
+
+
+
+
+
+while True:
+
+    
+    can_vel = can.get_vel(10)
+
+
+    print(can_vel)
+
