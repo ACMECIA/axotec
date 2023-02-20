42e18585642d8736de185e74dd3f6d64f5ad2eb6
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 17:49:45 -0500

    u

--- start.py
+++ start.py
@@ -1,18 +1,19 @@
 import canopen
 import time
+
 network = canopen.Network()
 
 network.connect(channel='can0', bustype='socketcan', bitrate=125000)
 
-# node = network.add_node(1, 'JN2100_V2.5.4.eds')     # El JN2100
+node = network.add_node(10, 'JN2100_V2.5.4.eds')     # El JN2100
 
-# for node_id in network:
-#     print(node_id)
-#     print(network[node_id])
+for node_id in network:
+    print(node_id)
+    print(network[node_id])
 
 # This will attempt to read an SDO from nodes 1 - 127
-network.scanner.search()
-# We may need to wait a short while here to allow all nodes to respond
-time.sleep(0.05)
-for node_id in network.scanner.nodes:
-    print("Found node %d!" % node_id)
+# network.scanner.search()
+# # We may need to wait a short while here to allow all nodes to respond
+# time.sleep(0.05)
+# for node_id in network.scanner.nodes:
+#     print("Found node %d!" % node_id)
