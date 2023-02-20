483bfa9b4da48731a3a25117f5a50bec0afce138
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 15:04:26 -0500

    u

--- test_can.py
+++ test_can.py
@@ -7,5 +7,6 @@ bus = can.interface.Bus(channel="can0", bustype='socketcan')
 # Lee los mensajes del bus CAN
 while True:
     message = bus.recv()
+    print("None")
     if message is not None:
         print(f"ID: {message.arbitration_id}  Data: {message.data}")
