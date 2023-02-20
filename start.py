f18481926c0331f194f60c6ff645f3690dcf0b40
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 17:33:17 -0500

    u

--- start.py
+++ start.py
@@ -1,13 +1,8 @@
 import canopen
 
-# Conectar al dispositivo CANopen
 network = canopen.Network()
-network.connect(bustype='socketcan', channel='can0', bitrate=125000)
-node_id = 10
-node = network.add_node(node_id, 'JN2100_V2.5.4.eds')
 
-# Escribir el valor 1 en el sub-índice 0x08 del índice 0x1F80
-print(node)
-node.sdo[0x1F80].write(0x08, 1)
-# Desconectar del dispositivo CANopen
-network.disconnect()
+network.connect(channel='can0', bustype='socketcan', bitrate=125000)
+
+for node_id in network:
+    print(network[node_id])
