37c9bff3aa5addf8ef82b63a86a7e4707f19f575
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 17:35:38 -0500

    u

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import canopen
 network = canopen.Network()
 
 network.connect(channel='can0', bustype='socketcan', bitrate=125000)
+node = network.add_node(6, 'JN2100_V2.5.4.eds')
 
 for node_id in network:
     print(node_id)
