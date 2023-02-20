180adad4c78065bfde1fb1536b57b9ecc435c418
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-20 18:55:20 -0500

    u

--- start.py
+++ start.py
@@ -20,12 +20,12 @@ for obj in node.object_dictionary.values():
 
 
 #print(node.object_dictionary[0x6010].value)
-print(node.sdo[0x6121].raw)
+#print(node.sdo[0x6121].raw)
 
 
 # This will attempt to read an SDO from nodes 1 - 127
-# network.scanner.search()
-# # We may need to wait a short while here to allow all nodes to respond
-# time.sleep(0.05)
-# for node_id in network.scanner.nodes:
-#     print("Found node %d!" % node_id)
+network.scanner.search()
+# We may need to wait a short while here to allow all nodes to respond
+time.sleep(0.05)
+for node_id in network.scanner.nodes:
+    print("Found node %d!" % node_id)
