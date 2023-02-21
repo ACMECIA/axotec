eb6b7117080b0ae0238e4a95ce62f7021b5d6602
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-21 16:07:39 -0500

    u

--- read.py
+++ read.py
@@ -6,4 +6,6 @@ bus = can.interface.Bus(channel='can1', bustype='socketcan')
 # Leer los mensajes CAN
 while True:
     message = bus.recv()
-    print(f"can{message.channel}  {message.arbitration_id:x}   [{message.dlc}]  {' '.join(f'{b:02x}' for b in message.data)}")
+    if message.arbitration_id == 0x2301:
+        print(message.data)
+    #print(f"can{message.channel}  {message.arbitration_id:x}   [{message.dlc}]  {' '.join(f'{b:02x}' for b in message.data)}")
