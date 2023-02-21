3120f78a77dd4ed370c1eb0694938ba0aaa52f4b
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-21 16:38:56 -0500

    u

--- read.py
+++ read.py
@@ -1,5 +1,31 @@
 import canopen
 
+import math
+
+def quaternion_to_euler(w,x,y,z):
+    # Convierte un cuaternión (w, x, y, z) a ángulos de Euler (pitch, roll, yaw)
+    # q: tupla (w, x, y, z) que representa el cuaternión
+    
+    # Normaliza el cuaternión
+
+    norm = math.sqrt(w**2 + x**2 + y**2 + z**2)
+    w /= norm
+    x /= norm
+    y /= norm
+    z /= norm
+    
+    # Calcula los ángulos de Euler
+    roll = math.atan2(2*(w*x + y*z), 1 - 2*(x**2 + y**2))
+    pitch = math.asin(2*(w*y - z*x))
+    yaw = math.atan2(2*(w*z + x*y), 1 - 2*(y**2 + z**2))
+    
+    # Convierte de radianes a grados
+    roll = math.degrees(roll)
+    pitch = math.degrees(pitch)
+    yaw = math.degrees(yaw)
+    
+    return pitch, roll, yaw
+
 # Cargar archivo de configuración de dispositivo CANopen
 network = canopen.Network()
 network.connect(bustype='socketcan', channel='can1')
@@ -17,15 +43,20 @@ index = 0x1000
 subindex = 1
 
 
+
+
 # device type: 0x1000
 # quaternion: 0x2301
 
 while True:
-    x_grav = node.sdo[0x2300][0x01]
-    y_grav = node.sdo[0x2300][0x02]
-    z_grav = node.sdo[0x2300][0x03]
+    w = node.sdo[0x2301][0x01].raw
+    x = node.sdo[0x2301][0x02].raw
+    y = node.sdo[0x2301][0x03].raw
+    z = node.sdo[0x2301][0x03].raw
 
-    print(x_grav.raw,y_grav.raw,z_grav.raw)
+    pitch,roll,yaw = quaternion_to_euler(w,x,y,z)
+
+    print(pitch,roll,yaw)
 
 
 # Cerrar conexión CAN
