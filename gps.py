52de190dd07ec3da5e307060dac4460059ff149d
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-23 17:22:39 -0500

    u

--- gps.py
+++ gps.py
@@ -1,13 +1,15 @@
 import serial
 
+knots2kmh = 1.852
+knots2ms = 0.514
+kmh2ms = 5/18
+
 class GPS():
     def __init__(self,port="/dev/ttyUSB1"):
         self.ser = serial.Serial(port)
         self.previous_speed = 0
         self.speed = 0
-        self.knots2kmh = 1.852
-        self.knots2ms = 0.514
-        self.kmh2ms = 5/18
+        
  
     def get_vel(self):
         
@@ -15,7 +17,7 @@ class GPS():
         GPVTG_Data = received_data.find(b"$GPVTG,")
         if (GPVTG_Data==0):
             speed_raw = received_data.split(b",")[7]       
-            self.speed = float(speed_raw)*self.kmh2ms#*self.knots2ms 
+            self.speed = float(speed_raw)*self.knots2ms 
             return self.speed
         self.previous_speed = self.speed
         return self.previous_speed
