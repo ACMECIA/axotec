fa35c146525af1a6e6d07c83edc09254814b8f87
Del Piero Flores <delpiero22.flores@gmail.com>
2023-02-23 17:36:02 -0500

    u

--- test_gps.py
+++ test_gps.py
@@ -3,7 +3,7 @@ from gps import *
 gps = GPS()
 
 while True:
-    
+
     vel_gps = gps.get_vel()
-    if gps.get_vel() is not None:
-        print(gps.get_vel())
+    if vel_gps is not None:
+        print(vel_gps)
