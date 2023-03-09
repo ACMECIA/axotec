33ebfe946b89853a47ba6205a7f74544f78b4160
Del Piero Flores <delpiero22.flores@gmail.com>
2023-03-09 16:49:30 -0500

    u

--- test_gps.py
+++ test_gps.py
@@ -3,7 +3,6 @@ from gps import *
 gps = GPS()
 
 while True:
-
+    gps.run()
     lat, long, alt = gps.get_pos()
-    if lat is not None:
-        print(f"Latitud: {lat}; Longitud: {long}; Altitud: {alt}")
+    print(f"Latitud: {lat}; Longitud: {long}; Altitud: {alt}")
